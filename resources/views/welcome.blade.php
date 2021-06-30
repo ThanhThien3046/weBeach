cbe9085fe25e7c4684f26e8a39fdc8cb7e3e52b7
thanh thien <thanhthien3046@gmail.com>
2021-06-30 15:04:44 +0900

    main

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    {{-- <link rel="preload" as="style" href="{{ asset('css/style.css' . Config::get('app.version'))}}"> --}}
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+        <div class="top-header">
+            <div class="col">
+                <span>PHONE: +01 256 25 235</span>
+                <span>EMAIL: INFO@EISER.COM</span>
+            </div>
+            <div class="col text-right">
+                <span>gift card</span>
+                <span>track order</span>
+            </div>
+        </div>
+        <div class="header-content">
+            
+        </div>
+        <span class="triangle"></span>
+    </div>
+</body>
+</html>
